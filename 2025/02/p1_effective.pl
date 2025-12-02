bcc479c85366703931102e4f2dbdca6d24271cdf
Filip Hasoň <hasonfilip@seznam.cz>
2025-12-02 15:51:55 +0100

    Add effective 25/02 part 1

---
+++ 2025/02/p1_effective.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub sum_repeated {
+	my ($a, $b) = @_;
+	my $sum_range = ($b - $a + 1) * ($a + $b) / 2;
+	my $multiplier = 10**length($a) + 1;
+	return $multiplier * $sum_range;
+}
+
+sub repeated {
+	my $number = shift;
+	return $number * (10**length($number) + 1);
+}
+
+sub part1 {
+	my $line = shift;
+	my $total = 0;
+
+	for my $pair (split /,/, $line) {
+		my ($start, $end) = split /-/, $pair;
+
+		for my $digits (length($start) .. length($end)) {
+			next if $digits % 2;
+
+			my $temp_start = (10**($digits-1) > $start) ? 10**($digits-1) : $start;
+			my $temp_end = (10**$digits - 1 < $end) ? 10**$digits - 1 : $end;
+
+			my $start_halved = substr($temp_start, 0, $digits / 2);
+			++$start_halved if repeated($start_halved) < $temp_start;
+
+			my $end_halved = substr($temp_end, 0, $digits / 2);
+			--$end_halved if repeated($end_halved) > $temp_end;
+
+			$total += sum_repeated($start_halved, $end_halved) 
+				if $start_halved <= $end_halved;
+		}
+	}
+	return $total;
+}
+
+my $line = <>;
+chomp $line;
+
+printf "Part 1: %d\n", part1($line);
