3e6ad3217c4738c9ebcc758457a158175f6d9b1a
Filip Hasoň <filip.hason@koala42.com>
2025-12-03 13:16:34 +0100

    Add 25/03

--- 2025/03/03.pl
+++ 2025/03/03.pl
@@ -2,25 +2,30 @@
 use strict;
 use warnings;
 
-my $part1 = 0;
-my $part2 = 0;
+my @lines = <>;
+chomp @lines;
 
-while (<>) {
-	chomp;
-	my @batteries = split //;
-	my $first = 0;
-	my $second = 0;
-
-	for (my $i = 0; $i < @batteries - 1; $i++) {
-		if ($batteries[$i] > $first) {
-			$first = $batteries[$i];
-			$second = 0;
+sub solve {
+	my $result = 0;
+	my $number_of_batteries = shift;
+	foreach my $line (@lines) {
+		my @batteries = split //, $line;
+		my @joltage = (0) x $number_of_batteries;
+		for (my $b = 0; $b < @batteries; $b++) {
+			my $start = $number_of_batteries - (@batteries - $b);
+			$start = 0 if $start < 0;
+			for (my $i = $start; $i < $number_of_batteries; $i++) {
+				if ($batteries[$b] > $joltage[$i]) {
+					$joltage[$i] = $batteries[$b];
+					$joltage[$_] = 0 for $i+1 .. $number_of_batteries-1;
+					last;
+				}
+			}
 		}
-		if ($batteries[$i+1] > $second) {
-			$second = $batteries[$i+1];
-		}
+		$result += join('', @joltage);
 	}
-	$part1 += $first * 10 + $second;
+	return $result;
 }
-print "Part 1: $part1\n";
-		
+
+print "Part 1: ", solve(2), "\n";
+print "Part 2: ", solve(12), "\n";
