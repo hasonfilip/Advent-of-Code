4d78dbcc765c99003b84c2365c44bf83dc0d6a18
Filip Hasoň <filip.hason@koala42.com>
2025-12-07 15:32:34 +0100

    Add 25/07

--- 2025/07/07.pl
+++ 2025/07/07.pl
@@ -1,19 +1,32 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use 5.010;
 use List::Util qw(first);
 
-#my @map;
 my %beams;
+my $part1 = 0;
+my $part2 = 1;
 while (<>) {
-    chomp;
-    my @chars = split //;
-    if (/S/) {
-      #push @beams, first { $chars[$_] eq 'S' } 0..$#chars;
-      my $pos = first { $chars[$_] eq 'S' } 0..$#chars;
-      $beams{$pos} = 1;
-      print $pos, "\n";
-    } else {
-      #push @map, \@chars;
-    }
+	chomp;
+	my @chars = split //;
+	if (/S/) {
+		$beams{first { $chars[$_] eq 'S' } 0..$#chars} = 1;
+	} else {
+		my %new_beams;
+		foreach my $key (keys %beams) {
+			if ($chars[$key] eq '^') {
+				$part1++;
+				$part2 += $beams{$key};
+				$new_beams{$key-1} += $beams{$key};
+				$new_beams{$key+1} += $beams{$key};
+
+			} else {
+				$new_beams{$key} += $beams{$key};
+			}
+		}
+		%beams = %new_beams;
+	}
 }
+say $part1;
+say $part2;
